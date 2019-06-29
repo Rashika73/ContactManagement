06c633a0cb6a786ff902092b45a618607346ac83
Rashika73 <rashikakarki9841@gmail.com>
2019-06-29 12:15:57 +0545

    First-Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class ContactList extends Component{
+  render() {
+      const people=this.props.contacts;
+  
+  return React.createElement('ol',{className:'welcome-message'},
+      people.map((person,index) => (
+          <li key={index}>{person.name}</li>)
+          )
+          )
+
 }
+}
+
+
+
+class App extends Component{
+  render()
+  {
+    return(
+      <div className="App">
+    <ContactList contacts={[
+      {name:'Ram'},
+      {name:'Hari'},
+      {name:'Shyam'}
+    ]}/>
+    <ContactList 
+    contacts={[
+      {name:'Rajiv'},
+      {name:'Ravi'},
+      {name:'Krishna'}
+    ]}
+    />
+    
+    </div>)
+  }
+}
+
+
 
 export default App;
