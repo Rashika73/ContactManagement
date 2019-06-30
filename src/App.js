05f8dc18e0f2f112ee62eb499345d9d13e315ac9
Rashika73 <rashikakarki9841@gmail.com>
2019-06-30 09:29:54 +0545

    Created route and form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React,{Component} from 'react';
-import ListContacts from './ListContacts'
-import * as ContactsAPI from './utils/ContactsAPI'
-import CreateContact from './CreateContact'
-
+import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
+import {Route} from 'react-router-dom'
 
 
 
@@ -36,12 +36,38 @@ removeContact=(contact)=>{
   {
     return(
       <div className="Contacts">
+
+
+        {/** 
         {this.state.screen==='list'&&(
           <ListContacts onNavigate={()=>{this.setState({screen:'create'})}} onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
           )}
-          {this.state.screen==='create'&&(
+          */}
+
+  {/** if no match then the path is partial that is even if it partially matched it does display*/ }  
+
+
+<Route  exact path='/'  render={()=>(<ListContacts onNavigate={()=>{this.setState({screen:'create'})}} onDeleteContact={this.removeContact} contacts={this.state.contacts}/>)}/>
+
+
+
+
+          {/** {this.state.screen==='create'&&(
           <CreateContact/>
-          )}
+          )*/}
+
+
+
+<Route path='/create' render={()=><CreateContact/>}/>
+
+
+{/** 
+  Same as above 
+
+<Route path='/create' component={CreateContact}/>
+
+*/}
+
       </div>
      )
   }
